5d392438dcd32dca55ecd14c6275697b215d30a1
zoe <627832803@qq.com>
2016-02-25 21:18:11 +0800

    case1_wrong_zero

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -7,15 +7,23 @@ var expect = chai.expect;
 chai.use(sinonChai);
 
 var main = require("../lib/main.js");
-var num_list =[3,7,7];
+var wrong_num_list =[3,7,7];
+var wrong_zero_list =[3,0,7];
 
 describe("测试描述", function(){
     sinon.spy(console, 'log');
 
     it("case1_wrong_input", function(){
 
-        var result = main(3,num_list);
+        var result = main(3,wrong_num_list);
         var expect_string = 'wrong input';
+        expect(expect_string).to.equal(result);
+    });
+
+    it("case1_wrong_zero", function(){
+
+        var result = main(3,wrong_zero_list);
+        var expect_string = 'wrong_zero';
         expect(expect_string).to.equal(result);
     });
 
