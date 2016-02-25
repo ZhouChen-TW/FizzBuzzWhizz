64513ac7b262da48452496d4db81fac9c0e22070
zoe <627832803@qq.com>
2016-02-25 21:53:07 +0800

    case4_contains_one

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -165,5 +165,34 @@ describe("测试描述", function(){
         expect(expect_string).to.equal(result);
     });
 
+    it("case4_contains_one", function(){
+
+        var result = main(23,num_list);
+        var expect_string = '1\n'
+            + '2\n'
+            + 'Fizz\n'
+            + '4\n'
+            + 'Buzz\n'
+            + 'Fizz\n'
+            + 'Whizz\n'
+            + '8\n'
+            + 'Fizz\n'
+            + 'Buzz\n'
+            + '11\n'
+            + 'Fizz\n'
+            + 'Fizz\n'
+            + 'Whizz\n'
+            + 'FizzBuzz\n'
+            + '16\n'
+            + '17\n'
+            + 'Fizz\n'
+            + '19\n'
+            + 'Buzz\n'
+            + 'FizzWhizz\n'
+            + '22\n'
+            + 'Fizz\n';
+        expect(expect_string).to.equal(result);
+    });
+
 
 });
